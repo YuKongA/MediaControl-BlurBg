cd0ff27ceea82067450c7c97ad3ac9d7ca592ec2
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-03-05 17:11:33 +0800

    ci: Enable signing for workflow artifacts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import java.util.Properties
 
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
@@ -9,7 +10,6 @@ plugins {
 android {
     namespace = "top.yukonga.mediaControlBlur"
     compileSdk = 34
-
     defaultConfig {
         applicationId = namespace
         minSdk = 34
@@ -17,19 +17,38 @@ android {
         versionCode = 2128
         versionName = "2.1.28"
     }
-
+    val properties = Properties()
+    runCatching { properties.load(project.rootProject.file("local.properties").inputStream()) }
+    val keystorePath = properties.getProperty("KEYSTORE_PATH") ?: System.getenv("KEYSTORE_PATH")
+    val keystorePwd = properties.getProperty("KEYSTORE_PASS") ?: System.getenv("KEYSTORE_PASS")
+    val alias = properties.getProperty("KEY_ALIAS") ?: System.getenv("KEY_ALIAS")
+    val pwd = properties.getProperty("KEY_PASSWORD") ?: System.getenv("KEY_PASSWORD")
+    if (keystorePath != null) {
+        signingConfigs {
+            create("release") {
+                storeFile = file(keystorePath)
+                storePassword = keystorePwd
+                keyAlias = alias
+                keyPassword = pwd
+                enableV3Signing = true
+            }
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = true
-            proguardFiles("proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            if (keystorePath != null) signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            if (keystorePath != null) signingConfig = signingConfigs.getByName("release")
+            applicationIdSuffix = ".debug"
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
         freeCompilerArgs = listOf(
@@ -39,7 +58,6 @@ android {
             "-language-version=2.0",
         )
     }
-
     packaging {
         resources {
             excludes += "**"

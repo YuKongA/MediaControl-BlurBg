00160ae6c7cb5e696220d01d7af770c5b58cab5a
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-12-12 11:49:21 +0800

    保留进度条更改

--- app/src/main/kotlin/top/yukonga/mediaControlOpt/MainHook.kt
+++ app/src/main/kotlin/top/yukonga/mediaControlOpt/MainHook.kt
@@ -1,6 +1,7 @@
 package top.yukonga.mediaControlOpt
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
@@ -10,8 +11,15 @@ import android.graphics.PorterDuffXfermode
 import android.graphics.Rect
 import android.graphics.RectF
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ClipDrawable
+import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.Icon
+import android.graphics.drawable.LayerDrawable
+import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
 import com.github.kyuubiran.ezxhelper.ClassUtils.loadClassOrNull
 import com.github.kyuubiran.ezxhelper.EzXHelper
 import com.github.kyuubiran.ezxhelper.HookFactory.`-Static`.createAfterHook
@@ -20,6 +28,9 @@ import com.github.kyuubiran.ezxhelper.ObjectHelper.Companion.objectHelper
 import com.github.kyuubiran.ezxhelper.finders.MethodFinder.`-Static`.methodFinder
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.callbacks.XC_LoadPackage
+import top.yukonga.mediaControlOpt.utils.AppUtils.colorFilter
+import top.yukonga.mediaControlOpt.utils.AppUtils.dp
+import top.yukonga.mediaControlOpt.utils.AppUtils.isDarkMode
 
 class MainHook : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
@@ -29,10 +40,51 @@ class MainHook : IXposedHookLoadPackage {
             "com.android.systemui" -> {
                 try {
                     val miuiMediaControlPanel = loadClassOrNull("com.android.systemui.statusbar.notification.mediacontrol.MiuiMediaControlPanel")
+                    val mediaViewHolder = loadClassOrNull("com.android.systemui.media.controls.ui.view.MediaViewHolder")
+
+                    mediaViewHolder?.constructors?.first()?.createAfterHook {
+                        val seekBar = it.thisObject.objectHelper().getObjectOrNullAs<SeekBar>("seekBar")
+                        val backgroundDrawable = GradientDrawable().apply {
+                            color = ColorStateList(arrayOf(intArrayOf()), intArrayOf(Color.parseColor("#20ffffff")))
+                            cornerRadius = 7.dp.toFloat()
+                        }
+                        val onProgressDrawable = GradientDrawable().apply {
+                            color = ColorStateList(arrayOf(intArrayOf()), intArrayOf(Color.parseColor("#ffffffff")))
+                            cornerRadius = 7.dp.toFloat()
+                        }
+                        val layerDrawable = LayerDrawable(
+                            arrayOf(backgroundDrawable, ClipDrawable(onProgressDrawable, Gravity.START, ClipDrawable.HORIZONTAL))
+                        ).apply {
+                            val layerHeight = 7.dp
+
+                            val totalHeight = seekBar?.height ?: 0
+                            val topOffset = (totalHeight - layerHeight) / 2
+
+                            setLayerInset(0, 0, topOffset, 0, topOffset)
+                            setLayerInset(1, 0, topOffset, 0, topOffset)
+                        }
+                        seekBar?.progressDrawable = layerDrawable
+                    }
 
                     miuiMediaControlPanel?.methodFinder()?.filterByName("bindPlayer")?.first()?.createAfterHook {
                         val context = it.thisObject.objectHelper().getObjectOrNullUntilSuperclassAs<Context>("mContext") ?: return@createAfterHook
                         val mMediaViewHolder = it.thisObject.objectHelper().getObjectOrNullUntilSuperclass("mMediaViewHolder") ?: return@createAfterHook
+
+                        val appIcon = mMediaViewHolder.objectHelper().getObjectOrNullAs<ImageView>("appIcon")
+                        (appIcon?.parent as ViewGroup?)?.removeView(appIcon)
+
+                        val seekBar = mMediaViewHolder.objectHelper().getObjectOrNullAs<SeekBar>("seekBar")
+                        seekBar?.thumb?.colorFilter = colorFilter(Color.TRANSPARENT)
+
+                        val elapsedTimeView = mMediaViewHolder.objectHelper().getObjectOrNullAs<TextView>("elapsedTimeView")
+                        val totalTimeView = mMediaViewHolder.objectHelper().getObjectOrNullAs<TextView>("totalTimeView")
+                        val grey = if (isDarkMode(context)) Color.LTGRAY else Color.DKGRAY
+
+                        elapsedTimeView?.setTextColor(grey)
+                        totalTimeView?.setTextColor(grey)
+                        elapsedTimeView?.textSize = 12f
+                        totalTimeView?.textSize = 12f
+
                         val albumView = mMediaViewHolder.objectHelper().getObjectOrNullAs<ImageView>("albumView")
                         val artwork = it.args[0].objectHelper().getObjectOrNullAs<Icon>("artwork") ?: return@createAfterHook
                         val artworkLayer = artwork.loadDrawable(context) ?: return@createAfterHook
